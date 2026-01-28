617e9f27db2bc766f347aeb9f5894d1976dd7478
michaeljuren <michael16.uren@gmail.com>
2026-01-28 19:51:16 +0200

    Repository which extends JPA

---
+++ src/main/java/com/michaeljuren/mfa/respository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.michaeljuren.mfa.respository;
+
+import com.michaeljuren.mfa.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
