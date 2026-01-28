8d51d3ee12af82d4c050f0da100eeeaf07156c9a
michaeljuren <michael16.uren@gmail.com>
2026-01-28 19:52:31 +0200

    TOTP service which generates the QR code and verifies TOTP code from user.

---
+++ src/main/java/com/michaeljuren/mfa/service/TotpService.java
@@ -0,0 +1,64 @@
+package com.michaeljuren.mfa.service;
+
+import dev.samstevens.totp.code.*;
+import dev.samstevens.totp.exceptions.QrGenerationException;
+import dev.samstevens.totp.qr.QrData;
+import dev.samstevens.totp.qr.QrGenerator;
+import dev.samstevens.totp.qr.ZxingPngQrGenerator;
+import dev.samstevens.totp.secret.DefaultSecretGenerator;
+import dev.samstevens.totp.time.SystemTimeProvider;
+import dev.samstevens.totp.time.TimeProvider;
+import org.springframework.stereotype.Service;
+
+import static dev.samstevens.totp.util.Utils.getDataUriForImage;
+
+@Service
+public class TotpService {
+    private final DefaultSecretGenerator secretGenerator;
+    private final QrGenerator qrGenerator;
+    private final CodeVerifier codeVerifier;
+
+    public TotpService() {
+        this.secretGenerator = new DefaultSecretGenerator();
+        this.qrGenerator = new ZxingPngQrGenerator();
+
+        TimeProvider timeProvider = new SystemTimeProvider();
+        CodeGenerator codeGenerator = new DefaultCodeGenerator();
+        this.codeVerifier = new DefaultCodeVerifier(codeGenerator, timeProvider);
+    }
+
+    /**
+     * Generate a new secret for a user
+     */
+    public String generateSecret() {
+        return secretGenerator.generate();
+    }
+
+    /**
+     * Generate QR code URL for Google Authenticator
+     */
+    public String generateQrCodeImageUri(String secret, String username) {
+        QrData data = new QrData.Builder()
+                .label(username)
+                .secret(secret)
+                .issuer("SpringMFA App")
+                .algorithm(HashingAlgorithm.SHA1)
+                .digits(6)
+                .period(30)
+                .build();
+
+        try {
+            byte[] imageData = qrGenerator.generate(data);
+            return getDataUriForImage(imageData, qrGenerator.getImageMimeType());
+        } catch (QrGenerationException e) {
+            throw new RuntimeException("Failed to generate QR code", e);
+        }
+    }
+
+    /**
+     * Verify the TOTP code provided by user
+     */
+    public boolean verifyCode(String secret, String code) {
+        return codeVerifier.isValidCode(secret, code);
+    }
+}
